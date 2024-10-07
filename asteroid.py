7d19bd13b5af994d5e4e2d80f6fe083324a56ed7
rzhlm <118811618+rzhlm@users.noreply.github.com>
2024-10-07 15:20:00 +0200

    asteroids now split into pieces

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from circleshape import CircleShape
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x,y, radius):
@@ -9,4 +10,19 @@ class Asteroid(CircleShape):
          pygame.draw.circle(screen, "white", self.position, self.radius, 2)
     def update(self, dt):
         self.position += self.velocity * dt
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        ran_degree = random.uniform(20,50)
+        a = self.velocity.rotate(ran_degree)
+        b = self.velocity.rotate(-ran_degree)
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid.velocity = a * 1.2
+        asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid.velocity = b * 1.2
+
+
     
--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def main():
    for shot in shots:
     if asteroid.collision_detect(shot):
      shot.kill()
-     asteroid.kill()
+     asteroid.split()
 
   screen.fill((0,0,0))
   for object in drawable:
