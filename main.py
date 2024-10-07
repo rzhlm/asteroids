55289974b00a2db9e862299f28dd4cf1d18fb0f7
rzhlm <118811618+rzhlm@users.noreply.github.com>
2024-10-07 14:39:58 +0200

    grouped into drawables & updatables

--- main.py
+++ main.py
@@ -10,9 +10,14 @@ def main():
  pygame.init()
  screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
  clock = pygame.time.Clock()
+
+
+
+ updatable = pygame.sprite.Group()
+ drawable = pygame.sprite.Group()
+ Player.containers = (updatable, drawable)
  playr = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
  dt = 0
-
  
 
 
@@ -20,9 +25,15 @@ def main():
   for event in pygame.event.get():
    if event.type == pygame.QUIT:
     return
+  
+  
+  for object in updatable:
+   object.update(dt)
+  
   screen.fill((0,0,0))
-  playr.draw(screen)
-  playr.update(dt)
+  for object in drawable:
+   object.draw(screen)
+  
   pygame.display.flip()
   
   dt = clock.tick(60) / 1000
